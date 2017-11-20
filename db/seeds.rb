1ff413dcce2080a2e5b563c7972b848851fec45f
vitalyliber <zenamax@gmail.com>
2017-11-20 14:15:33 +0700

    create a book model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create [
+    {
+        title: 'Harry Potter and the Philosopher\'s Stone',
+        author: 'Joanne Kathleen Rowling',
+        desc: '...'
+    },
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'Joanne Kathleen Rowling',
+        desc: '...'
+    },
+    {
+        title: 'Harry Potter and the Prisoner of Azkaban',
+        author: 'Joanne Kathleen Rowling',
+        desc: '...'
+    },
+    {
+        title: 'Harry Potter and the Goblet of Fire',
+        author: 'Joanne Kathleen Rowling',
+        desc: '...'
+    },
+]
